14799c44bfcd27f7307a8858eb4236ad34105143
Micah Geisel <micah@botandrose.com>
2015-12-15 12:09:11 -0800

    refactor bookmarklet.js.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,81 +1,52 @@
-$("#message-input").keydown(function(event) {
-  if(event.keyCode == 9) {
-    var message = $(this).val();
-    for(var macroName in macros) {
-      var macro = macros[macroName];
-      message = message.replace(macro.pattern, function() {
+var SlackMacro = function(pattern, macro) {
+  $("#message-input").keydown(function(event) {
+    if(event.keyCode !== 9) return;
+
+    $(this).val(function(index, message) {
+      return message.replace(pattern, function() {
         var matches = Array.prototype.slice.call(arguments, 1);
-        return macro.macro.apply(window, matches);
+        return macro.apply(window, matches);
       });
+    });
+  });
+};
+
+new SlackMacro(/\/frogonacci (\d+)/g, function(lines) {
+  return frogonacci(parseInt(lines));
+
+  function frogonacci(lines) {
+    var frogs = [
+      ':rainbowfrog:',
+      ':rainbowfrog2:',
+      ':rainbowfrog3:',
+      ':rainbowfrogflip:',
+      ':rainbowfroglsd:',
+      ':rainbowfrogmellow:',
+      ':rainbowfrogweyes:',
+      ':reverserainbowfrog:',
+      ':turbofrog:',
+      ':turboflipfrog:',
+      ':upsidedownrainbowfrog:',
+    ];
+
+    frogs.random = function() {
+      return this[Math.floor((Math.random()*this.length))];
+    };
+
+    return new Array(lines).fill(0).map(function(_, index) {
+      var count = fibonacci(index + 1);
+      return randomFrogs(count);
+    }).join("\n");
+
+    function fibonacci(n) {
+      return n <= 2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
     }
-    $(this).val(message);
+
+    function randomFrogs(count) {
+      return new Array(count).fill(0).map(function() {
+        return frogs.random();
+      }).join(' ');
+    }
   }
 });
 
-var macros = {
-  frogonacci: {
-    pattern: /\/frogonacci (\d+)/g,
-    macro: function(qty) {
-      return frogonacci(parseInt(qty));
-    }
-  }
-}
-
-function frogonacci(qty) {
-  // an array of every frog
-  var frogArray = [
-      'rainbowfrog',
-      'rainbowfrog2',
-      'rainbowfrog3',
-      'rainbowfrogflip',
-      'rainbowfroglsd',
-      'rainbowfrogmellow',
-      'rainbowfrogweyes',
-      'reverserainbowfrog',
-      'turbofrog',
-      'turboflipfrog',
-      'upsidedownrainbowfrog'
-  ];
-  
-  function fibonacciFrog(qty) {
-    var frogs = [];
-  
-    // Push sequence into an array
-    for(var i = 1; i <= qty; i++) {
-      frogs.push(fibonacci(i));
-    }
-  
-    // Frogify the array and print it out
-    return frogify(frogs);
-  }
-  
-  // Convert each fibonacci number to
-  function frogify(numArray) {
-    return numArray.map(function(currentNum) {
-      var currentLine = '';
-  
-      for(var i = 0; i < currentNum; i++) {
-        currentLine += ':' + randomFrog() + ': ';
-      }
-      return currentLine;
-    }).join(' ');
-  }
-  
-  // Basic recursive fibonacci function
-  function fibonacci(n) {
-      if (n <= 2) {
-          return 1;
-      }
-  
-      else {
-        return fibonacci(n-1) + fibonacci(n-2);
-      }
-  }
-
-  // Get random frog from array
-  function randomFrog() {
-      return frogArray[Math.floor((Math.random() * frogArray.length))];
-  }
-  
-  return fibonacciFrog(qty);
-}
